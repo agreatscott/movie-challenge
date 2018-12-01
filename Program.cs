fad7629d523214e600b2923b8b28f3e4dbb1c805
Alex S <alexanderbjscott@gmail.com>
2018-11-30 21:01:15 -0800

    Added initial file reading
    Added basic file reading logic and added some given parameters to the
    config.

--- Program.cs
+++ Program.cs
@@ -19,6 +19,15 @@ namespace MoviesChallenge
             var weeklyHoursOfOperation = new WeeklyHoursOfOperation();
             configuration.GetSection("HoursOfOperation").Bind(weeklyHoursOfOperation);
 
+            
+            using (var inputStream = File.OpenText(args[0]))
+            {
+                string line;
+                while ((line = inputStream.ReadLine()) != null) {
+
+                    Console.WriteLine(line);
+                }
+            }
             Console.WriteLine("Hello World!");
         }
     }
---
+++ models/MovieDataItem.cs
@@ -0,0 +1,11 @@
+namespace MoviesChallenge.Models
+{
+    class MovieDataItem
+    {
+        private string Title { get; set; }
+        private string ReleaseYear { get; set; }
+        private string Rating { get; set; }
+        private int RuntimeMinutes { get; set; }
+    }
+
+}
